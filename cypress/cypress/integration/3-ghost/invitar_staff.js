31e8121ee8ccd9ac6e30e7beb2dd81db1231e297
jaalruta <ja.ruedat1@uniandes.edu.co>
2022-05-05 23:51:55 -0500

    se agrega usuario por defecto en cypress

--- cypress/cypress/integration/3-ghost/invitar_staff.js
+++ cypress/cypress/integration/3-ghost/invitar_staff.js
@@ -6,8 +6,8 @@ describe ('Invitar staff', function(){
 
     it ('invitar staff', function(){
         cy.visit("http://localhost:2368/ghost/#/signin");
-        cy.get('#ember7').type('pruebas@ghost.com');
-        cy.get('#ember9').type('Pruebas12345');
+        cy.get('#ember7').type(Cypress.config('usuario'));
+        cy.get('#ember9').type(Cypress.config('pass'));
         cy.get('#ember11').click();
         cy.wait(5000);
         cy.get(".gh-nav-body a[href='#/settings/']").click();
