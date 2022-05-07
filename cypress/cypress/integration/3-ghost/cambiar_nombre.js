c293c7728a1e83e9591717f9b651fe7bd8c564f0
Juan Esteban <j.otalvaro@uniandes.edu.co>
2022-05-06 19:51:23 -0500

    cypress JE vs3

--- cypress/cypress/integration/3-ghost/cambiar_nombre.js
+++ cypress/cypress/integration/3-ghost/cambiar_nombre.js
@@ -4,11 +4,12 @@ describe ('crear', function(){
         cy.get('#ember7').type('j.otalvaro@uniandes.edu.co');
         cy.get('#ember9').type('irchete7811');
         cy.get('#ember11 > span').click();
+        cy.wait(1000);
         cy.get(".gh-nav-body a[href='#/settings/']").click();
         cy.wait(1000);
         cy.get(".view-container a[href='#/settings/general/']").click();
         cy.get(':nth-child(2) > .gh-expandable > :nth-child(1) > .gh-expandable-header > .gh-btn > span').click();
-        cy.get('#ember100').type(' MISO')
+        cy.get('#ember100').type(' MISO');
         cy.get('#ember68 > span').click();
     })
 })
